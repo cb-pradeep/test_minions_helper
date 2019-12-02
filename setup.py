6a9def287572e823e99d9badf93d6955ba7e7a03
Nitin <44866437+cb-nitin@users.noreply.github.com>
2019-12-02 19:26:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cb-infra-minion-helpers",
+    name="minions_core",
     version="0.0.3",
     author="Mohan Rajan R",
     author_email="mohan.r@chargebee.com",
